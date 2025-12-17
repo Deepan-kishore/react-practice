f5bb24873112a5c7abc1d0b63ee99ef1bd07bc1c
Deepan kishore <Deepan.Kishore@american-equity.com>
2025-12-17 16:37:36 +0530

    creating app from next js documentation

---
+++ nextjs-demo-app-dashboard/app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import { Inter , Lusitana} from "next/font/google";
+
+export const inter = Inter({
+    subsets:['latin']
+})
+
+export const lusitana_regular =  Lusitana({
+    weight:'400'
+})
+export const lusitana =  Lusitana({
+    weight:'400'
+})
+
+
+export const lusitana_bold =  Lusitana({
+    weight:'700'
+})
